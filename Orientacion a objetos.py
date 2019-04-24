9d453e248b009c2e6c7bb1c0ecff3068f1e8921d
Claudia Portal <clawportal@gmail.com>
2019-04-24 16:52:17 -0400

    orientacion a objetos

---
+++ Orientacion a objetos.py
@@ -0,0 +1,14 @@
+# en el archivo persona.py crear una clase Persona con atributo nombre
+#despues instanciar eun objeto de tipo persona
+
+class Persona:
+    nombre = None
+    edad = None
+    cumpleanhos = None
+    def __init__ (self, nombre, edad ,cumpleanhos):
+        self.nombre = el_nombre
+        self.edad = la_edad
+        self.cumpleanhos = fecha_de_cumpleanhos
+        print ("hola me llamo", self.nombre, "tengo", self.edad, "mi cumpleanhos es en", self.cumpleanhos)
+yo = Persona ("fulano", 400, "30 de julio")
+
