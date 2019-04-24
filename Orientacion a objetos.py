8e8998ee97943a99c038b80dd92105c929eb4157
Claudia Portal <clawportal@gmail.com>
2019-04-24 17:01:30 -0400

    correccion de errores en class persona

--- Orientacion a objetos.py
+++ Orientacion a objetos.py
@@ -5,10 +5,10 @@ class Persona:
     nombre = None
     edad = None
     cumpleanhos = None
-    def __init__ (self, nombre, edad ,cumpleanhos):
+    def __init__ (self, el_nombre, la_edad ,el_cumpleanhos):
         self.nombre = el_nombre
         self.edad = la_edad
-        self.cumpleanhos = fecha_de_cumpleanhos
-        print ("hola me llamo", self.nombre, "tengo", self.edad, "mi cumpleanhos es en", self.cumpleanhos)
+        self.cumpleanhos = el_cumpleanhos
+        print ("hola me llamo", self.nombre, "tengo", self.edad,"anhos", "mi cumpleanhos es en", self.cumpleanhos)
 yo = Persona ("fulano", 400, "30 de julio")
 
