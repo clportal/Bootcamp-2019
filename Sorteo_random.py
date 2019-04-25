5790db9eea216c845bf2ae7e854c02a40705d480
SantiagoAvila <49822653+SantiagoAvila@users.noreply.github.com>
2019-04-25 11:37:14 -0400

    Update Sorteo_random.py

--- Sorteo_random.py
+++ Sorteo_random.py
@@ -16,4 +16,16 @@ for sorteo in  range (3):
     parti.pop(un_random)
     print ("sorteo",sorteo + 1, ":", ganador)
 
-
+##### teniendo una lista hecha puedes utilizar esta funcion definir numero de premios y realizar el sorteo  
+def sorteos(cantidad_premios,lista):
+    if cantidad_premios <= len(lista):
+        sorteo=1
+        while sorteo<=cantidad_premios:
+            x=len(lista)-1
+            y=randint(0,x)
+            z= lista[y]
+            print("el ganador numero",sorteo,"es",z)
+            lista.pop(y)
+            sorteo=sorteo+1
+    else:
+        print("tu numero de premios supera al de participantes... dejate de joder")
